3275b72f5e0e07fd573dcf9640e2a97b41a8264d
JoelPMrz <joelperezmartinez01@gmail.com>
2024-10-28 22:21:01 +0100

    clean

--- app/src/main/java/com/example/products/MainActivity.kt
+++ app/src/main/java/com/example/products/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.products
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
